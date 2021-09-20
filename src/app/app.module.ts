c97be4757e14bbb9cc950501db9cc406d82e2a68
Abdulaziz Alnuaimi <abdulaziz.alneaimi@gmail.com>
2021-09-20 12:25:23 +0400

    ++

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 // ! https://mherman.org/blog/authentication-in-angular-with-ngrx/
+// * Next: HTTP Interceptor and Router Gaurd
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
